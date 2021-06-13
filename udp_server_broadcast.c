82d81afaf10a5deb8bee10cfc21a5991fa53dc43
Murtadha Bazli Tukimat <murtadhabazlitukimat@gmail.com>
2021-06-14 04:13:24 +0800

    Add program to response to broadcast message with its own IP address

---
+++ udp_server_broadcast.c
@@ -0,0 +1,77 @@
+/**
+ * @brief   This program response to broadcasted message from host
+ * 
+ * @author  Murtadha Bazli Tukimat (murtadhabazlitukimat@gmail.com)
+ * @date    14th June 2021
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netdb.h>
+
+#define BROADCAST_UDP_PORT  1112
+
+int error_handler(int sock, char* source)
+{
+    perror(source);
+    close(sock);
+    return 1;
+}
+
+int main(void)
+{
+    int socked = socket(AF_INET, SOCK_DGRAM, 0);
+    if(socked < 0)
+    {
+        return error_handler(socked, "socket");
+    }
+
+    // add socket option for broadcast
+    int rc = 1;
+    if(setsockopt(socked, SOL_SOCKET, SO_BROADCAST, &rc, sizeof(rc)) < 0)
+    {
+        return error_handler(socked, "setsockopt");
+    }
+
+    // bind to port 1112
+    struct sockaddr_in recv_addr;
+    recv_addr.sin_family = AF_INET;
+    recv_addr.sin_port = htons(BROADCAST_UDP_PORT);
+    recv_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(socked, (struct sockaddr*) &recv_addr, sizeof(recv_addr)) < 0)
+    {
+        return error_handler(socked, "bind");
+    }
+
+    struct sockaddr_in send_addr;
+    char recv_buffer[25] = {0};
+    int len = sizeof(send_addr);
+    if(recvfrom(socked, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr*) &send_addr, &len) < 0)
+    {
+        return error_handler(socked, "recvfrom");
+    }
+
+    // get device IP address from pre-defined interface
+    struct ifreq ifr;
+    ifr.ifr_addr.sa_family = AF_INET;
+    strncpy(ifr.ifr_name, "wlp3s0", (IFNAMSIZ-1));
+    ioctl(socked, SIOCGIFADDR, &ifr);
+    printf("response broadcast with IP addr: %s\n", inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->sin_addr));
+
+    char* send_buffer = inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->sin_addr);
+    if(sendto(socked, send_buffer, strlen(send_buffer), 0, (struct sockaddr*) &send_addr, sizeof(send_addr)) < 0)
+    {
+        return error_handler(socked, "sendto");
+    }
+
+    return 0;
+}
